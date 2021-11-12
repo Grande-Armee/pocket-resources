a1f3cfa90c2664a2177301adbe25411cb6d2fb7e
michalovsky <michalovskyyy@gmail.com>
2021-11-12 13:23:20 +0100

    feat: add tag repo crud

--- src/app/domain/tag/repositories/tag/tag.repository.ts
+++ src/app/domain/tag/repositories/tag/tag.repository.ts
@@ -10,8 +10,16 @@ import { TagMapper } from '../../mappers/tag/tag.mapper';
 export class TagRepository {
   public constructor(private readonly manager: EntityManager, private readonly tagMapper: TagMapper) {}
 
-  public async findOneById(id: string): Promise<TagDTO | null> {
-    const tag = await this.manager.findOne(Tag, { id });
+  public async createOne(tagData: Partial<Tag>): Promise<TagDTO> {
+    const tag = this.manager.create(Tag, { ...tagData });
+
+    const [savedTag] = await this.manager.save([tag]);
+
+    return this.tagMapper.mapEntityToDTO(savedTag);
+  }
+
+  public async findOne(tagData: Partial<Tag>): Promise<TagDTO | null> {
+    const tag = await this.manager.findOne(Tag, { ...tagData });
 
     if (!tag) {
       return null;
@@ -20,18 +28,32 @@ export class TagRepository {
     return this.tagMapper.mapEntityToDTO(tag);
   }
 
-  public async createOne(): Promise<TagDTO> {
-    // TODO: remove hardcoded id
-    const tag = this.manager.create(Tag, {
-      id: 'ef492cef-c478-4974-8555-97adadcc5c15',
-      color: 'asd',
-      title: 'asd',
-      userId: 'a6a18f2d-46f6-47e9-8c5d-ce7352fa22d5',
-    });
+  public async findAll(): Promise<TagDTO[]> {
+    const tags = await this.manager.find(Tag, {});
 
-    const [savedTag] = await this.manager.save([tag]);
+    return tags.map((tag) => this.tagMapper.mapEntityToDTO(tag));
+  }
 
-    return this.tagMapper.mapEntityToDTO(savedTag);
+  public async updateOne(id: string, data: Partial<Tag>): Promise<TagDTO> {
+    const tag = await this.findOne({ id });
+
+    if (!tag) {
+      throw new Error('Tag not found');
+    }
+
+    await this.manager.update(Tag, { id }, data);
+
+    return this.findOne({ id }) as Promise<TagDTO>;
+  }
+
+  public async removeOne(id: string): Promise<void> {
+    const tag = await this.findOne({ id });
+
+    if (!tag) {
+      throw new Error('Tag not found');
+    }
+
+    await this.manager.delete(Tag, { id });
   }
 }
 
