9e563908bca641034e5582383faf2f4261d29e8a
michalovsky <michalovskyyy@gmail.com>
2021-11-13 20:54:45 +0100

    feat: add missing user-resource test with optional dto fields

--- src/app/domain/user-resource/mappers/user-resource/user-resource.mapper.integration-spec.ts
+++ src/app/domain/user-resource/mappers/user-resource/user-resource.mapper.integration-spec.ts
@@ -8,6 +8,7 @@ import { Tag } from '../../../tag/entities/tag.entity';
 import { TagTestFactory } from '../../../tag/tests-factories/tag.factory';
 import { UserResourceTag } from '../../../user-resource-tag/entities/user-resource-tag.entity';
 import { UserResource } from '../../../user-resource/entities/user-resource.entity';
+import { UserResourceTestFactory } from '../../tests-factories/user-resource.factory';
 import { ResourceMapper } from './../../../resource/mappers/resource/resource.mapper';
 import { TagMapper } from './../../../tag/mappers/tag/tag.mapper';
 import { UserResourceMapper } from './user-resource.mapper';
@@ -133,6 +134,57 @@ describe('UserResourceMapper', () => {
           isFavorite: false,
           rating: null,
           status: 'TO_READ',
+          resourceId: resource.id,
+          userId: userId,
+          resource: resourceMapper.mapEntityToDTO(savedResource),
+          tags: [],
+        });
+      });
+    });
+
+    it('maps a resource with optional fields to dto', async () => {
+      expect.assertions(1);
+
+      await postgresHelper.runInTestTransaction(async (unitOfWork) => {
+        const entityManager = unitOfWork.getEntityManager();
+
+        const url = ResourceTestFactory.createUrl();
+        const userId = ResourceTestFactory.createId();
+        const isFavorite = UserResourceTestFactory.createIsFavorite();
+        const rating = UserResourceTestFactory.createRating();
+        const status = UserResourceTestFactory.createStatus();
+
+        const resource = entityManager.create(Resource, { url });
+
+        const [savedResource] = await entityManager.save([resource]);
+
+        const userResource = entityManager.create(UserResource, {
+          userId: userId,
+          resourceId: resource.id,
+        });
+
+        await entityManager.save([userResource]);
+
+        await entityManager.update(UserResource, { id: userResource.id }, { isFavorite, rating, status });
+
+        const queryBuilder = entityManager.getRepository(UserResource).createQueryBuilder('userResource');
+
+        const userResourceEntity = await queryBuilder
+          .leftJoinAndSelect('userResource.resource', 'resource')
+          .leftJoinAndSelect('userResource.userResourceTags', 'userResourceTags')
+          .leftJoinAndSelect('userResourceTags.tag', 'tag')
+          .where({ id: userResource.id })
+          .getOne();
+
+        const userResourceDTO = userResourceMapper.mapEntityToDTO(userResourceEntity as UserResource);
+
+        expect(userResourceDTO).toEqual({
+          id: userResource.id,
+          createdAt: userResource.createdAt,
+          updatedAt: userResource.updatedAt,
+          isFavorite: isFavorite,
+          rating: rating,
+          status: status,
           resourceId: resource.id,
           userId: userId,
           resource: resourceMapper.mapEntityToDTO(savedResource),
