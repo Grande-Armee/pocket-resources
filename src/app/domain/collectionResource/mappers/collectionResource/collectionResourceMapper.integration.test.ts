7c94cda7a6d08e85dc5388b585a9258fd41de064
michalovsky <michalovskyyy@gmail.com>
2021-11-16 21:14:20 +0100

    feat: add collectionResource entity to dto mapper

---
+++ src/app/domain/collectionResource/mappers/collectionResource/collectionResourceMapper.integration.test.ts
@@ -0,0 +1,75 @@
+import { TestingModule } from '@nestjs/testing';
+
+import { Collection } from '@domain/collection/entities/collection';
+import { CollectionTestDataGenerator } from '@domain/collection/testDataGenerators/collectionTestDataGenerator';
+import { Resource } from '@domain/resource/entities/resource';
+import { ResourceTestDataGenerator } from '@domain/resource/testDataGenerators/resourceTestDataGenerator';
+import { PostgresHelper } from '@integration/helpers/postgresHelper/postgresHelper';
+import { TestModuleHelper } from '@integration/helpers/testModuleHelper/testModuleHelper';
+
+import { CollectionResource } from '../../entities/collectionResource';
+import { CollectionResourceMapper } from './collectionResourceMapper';
+
+describe('CollectionResourceMapper', () => {
+  let testingModule: TestingModule;
+  let postgresHelper: PostgresHelper;
+  let resourceTestDataGenerator: ResourceTestDataGenerator;
+  let collectionTestDataGenerator: CollectionTestDataGenerator;
+
+  let collectionResourceMapper: CollectionResourceMapper;
+
+  beforeEach(async () => {
+    testingModule = await TestModuleHelper.createTestingModule();
+    postgresHelper = new PostgresHelper(testingModule);
+    collectionTestDataGenerator = new CollectionTestDataGenerator();
+    resourceTestDataGenerator = new ResourceTestDataGenerator();
+
+    collectionResourceMapper = testingModule.get(CollectionResourceMapper);
+  });
+
+  afterEach(async () => {
+    await testingModule.close();
+  });
+
+  describe('Map collection-resource', () => {
+    it('maps a collection-resource from entity to dto', async () => {
+      expect.assertions(1);
+
+      await postgresHelper.runInTestTransaction(async (unitOfWork) => {
+        const entityManager = unitOfWork.getEntityManager();
+
+        const { url } = resourceTestDataGenerator.generateEntityData();
+        const { userId } = collectionTestDataGenerator.generateEntityData();
+
+        const resource = entityManager.create(Resource, { url });
+
+        await entityManager.save([resource]);
+
+        const collection = entityManager.create(Collection, {
+          userId: userId,
+        });
+
+        await entityManager.save([collection]);
+
+        const collectionResource = entityManager.create(CollectionResource, {
+          collectionId: collection.id,
+          resourceId: resource.id,
+        });
+
+        const [savedCollectionResource] = await entityManager.save([collectionResource]);
+
+        const collectionResourceDto = collectionResourceMapper.mapEntityToDto(
+          savedCollectionResource as CollectionResource,
+        );
+
+        expect(collectionResourceDto).toEqual({
+          id: savedCollectionResource.id,
+          createdAt: savedCollectionResource.createdAt,
+          updatedAt: savedCollectionResource.updatedAt,
+          collectionId: collection.id,
+          resourceId: resource.id,
+        });
+      });
+    });
+  });
+});
---
+++ src/app/domain/collectionResource/mappers/collectionResource/collectionResourceMapper.ts
@@ -0,0 +1,22 @@
+import { DtoFactory, Mapper } from '@grande-armee/pocket-common';
+import { Injectable } from '@nestjs/common';
+
+import { CollectionResourceDto } from '../../dtos/collectionResourceDto';
+import { CollectionResource } from '../../entities/collectionResource';
+
+@Injectable()
+export class CollectionResourceMapper implements Mapper<CollectionResource, CollectionResourceDto> {
+  public constructor(private readonly dtoFactory: DtoFactory) {}
+
+  public mapEntityToDto(entity: CollectionResource): CollectionResourceDto {
+    const { id, createdAt, updatedAt, collectionId, resourceId } = entity;
+
+    return this.dtoFactory.createDtoInstance(CollectionResourceDto, {
+      id,
+      createdAt,
+      updatedAt,
+      collectionId,
+      resourceId,
+    });
+  }
+}
