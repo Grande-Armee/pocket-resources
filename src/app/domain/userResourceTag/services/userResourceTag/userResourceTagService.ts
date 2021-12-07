128622e9c83eecb8992aad3d500d333029280ad1
michalovsky <michalovskyyy@gmail.com>
2021-12-07 22:57:59 +0100

    feat: add logs to userResourceTag service

--- src/app/domain/userResourceTag/services/userResourceTag/userResourceTagService.ts
+++ src/app/domain/userResourceTag/services/userResourceTag/userResourceTagService.ts
@@ -1,3 +1,4 @@
+import { LoggerService } from '@grande-armee/pocket-common';
 import { Injectable } from '@nestjs/common';
 
 import { PostgresUnitOfWork } from '@shared/unitOfWork/providers/unitOfWorkFactory';
@@ -8,7 +9,10 @@ import { CreateUserResourceTagData, FindUserResourceTagData } from './interfaces
 
 @Injectable()
 export class UserResourceTagService {
-  public constructor(private readonly userResourceTagRepositoryFactory: UserResourceTagRepositoryFactory) {}
+  public constructor(
+    private readonly userResourceTagRepositoryFactory: UserResourceTagRepositoryFactory,
+    private readonly logger: LoggerService,
+  ) {}
 
   public async findUserResourceTag(
     unitOfWork: PostgresUnitOfWork,
@@ -33,10 +37,18 @@ export class UserResourceTagService {
     unitOfWork: PostgresUnitOfWork,
     userResourceTagData: CreateUserResourceTagData,
   ): Promise<UserResourceTagDto> {
+    this.logger.debug('Creating user resource tag...', {
+      userId: userResourceTagData.userId,
+      resourceId: userResourceTagData.resourceId,
+      tagId: userResourceTagData.tagId,
+    });
+
     const { entityManager } = unitOfWork;
     const userResourceTagRepository = this.userResourceTagRepositoryFactory.create(entityManager);
 
     const userResourceTag = await userResourceTagRepository.createOne(userResourceTagData);
+
+    this.logger.info('User resource tag created.', { userResourceTagId: userResourceTag.id });
 
     return userResourceTag;
   }
@@ -45,11 +57,16 @@ export class UserResourceTagService {
     unitOfWork: PostgresUnitOfWork,
     userResourceTagData: FindUserResourceTagData,
   ): Promise<void> {
+    this.logger.debug('Removing user resource tag...', {
+      userId: userResourceTagData.userId,
+      resourceId: userResourceTagData.resourceId,
+      tagId: userResourceTagData.tagId,
+    });
+
     const { entityManager } = unitOfWork;
     const userResourceTagRepository = this.userResourceTagRepositoryFactory.create(entityManager);
 
     // TODO: add finding by userId, resourceId and tagId instead of userResourceId and tagId
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { resourceId, tagId } = userResourceTagData;
 
     const userResourceTag = await userResourceTagRepository.findOne({ userResourceId: resourceId, tagId });
@@ -57,6 +74,8 @@ export class UserResourceTagService {
     if (!userResourceTag) {
       throw new Error('User resource tag not found.');
     }
+
+    this.logger.info('User resource tag removed.', { userResourceTagId: userResourceTag.id });
 
     await userResourceTagRepository.removeOne(userResourceTag.id);
   }
